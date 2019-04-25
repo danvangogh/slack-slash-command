2043686a7d7cfaca84236193d22df70180ccb794
danvangogh <djameswhite@yahoo.com>
2019-04-24 20:40:06 -0700

    init

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const sayings = [
+  "Grrrrreat!",
+  "You're not wrong.",
+  "Glorious",
+  "Kids these days, I tell ya.",
+  "{{ probably_a_dad_joke }}",
+]
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/jotham', (req, res) => {
+  res.send(randomizer(sayings))
+})
+
+app.post('/jotham', (req, res) => {
+  res.send(randomizer(sayings))
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+randomizer = (arr) => {
+  let length = arr.length;
+  let index = Math.floor(Math.random() * length);
+  return arr[index];
+}
